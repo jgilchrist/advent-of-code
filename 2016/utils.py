5e4b7952e19d1985a8c4f090e268d3dcf2b14581
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-12-02 10:56:29 +0000

    Move direction_offset into utils for reusability

--- 2016/utils.py
+++ 2016/utils.py
@@ -4,3 +4,20 @@ def tuple_add(a, b):
 
 def tuple_mul(tup, n):
     return tuple(i * n for i in tup)
+
+def direction_offset(d):
+    """This method is a reusable map from directional enums to offsets.
+    The only caveat is that it requires enums to be IntEnum for int
+    comparisons, and that the fields of the enum must be defined in
+    compass order."""
+
+    return {
+        # Up
+        1: ( 0, -1),
+        # Down
+        3: ( 0,  1),
+        # Left
+        4: (-1,  0),
+        # Right
+        2: ( 1,  0),
+    }[d]
