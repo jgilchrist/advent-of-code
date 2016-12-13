ccc5067e2fc8488c1ab332ce2d7e00612ce6a279
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-12-13 10:39:51 +0000

    Rename single-digit challenges to use padded zeroes

--- 2016/test_utils.py
+++ 2016/test_utils.py
@@ -1,11 +1,11 @@
-from utils import read_input
+from utils import import_challenge, read_input
 
 def Part1(challenge_number, answer, skip=False):
     import unittest
 
     def test_part1(self):
         """Part 1"""
-        challenge = __import__(str(challenge_number))
+        challenge = import_challenge(challenge_number)
         challenge_input = read_input(challenge_number)
         result = challenge.part1(challenge_input)
         self.assertEqual(result, answer)
@@ -22,7 +22,7 @@ def Part2(challenge_number, answer, skip=False):
 
     def test_part2(self):
         """Part 2"""
-        challenge = __import__(str(challenge_number))
+        challenge = import_challenge(challenge_number)
         challenge_input = read_input(challenge_number)
         result = challenge.part2(challenge_input)
         self.assertEqual(result, answer)
--- 2016/utils.py
+++ 2016/utils.py
@@ -51,11 +51,13 @@ def zip_with_constant(const, iterable):
 def concat(iterable):
     return "".join(iterable)
 
+def import_challenge(challenge_number):
+    return __import__(str(challenge_number).zfill(2))
 
 def read_input(challenge_number):
-    challenge = __import__(str(challenge_number))
+    challenge = import_challenge(challenge_number)
 
-    filename = "{}.input".format(challenge_number)
+    filename = "{0:02d}.input".format(challenge_number)
 
     with open(filename) as f:
         challenge_input = f.read().strip()
