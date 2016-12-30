672246fb46be8932c86c3db2b20700ca500d3cce
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-12-30 15:50:16 +0000

    Rename testcases to be 0-padded

--- 2016/test.py
+++ 2016/test.py
@@ -4,31 +4,31 @@ from test_utils import Part1, Part2
 
 @Part1(1, 241)
 @Part2(1, 116)
-class TestDay1(unittest.TestCase): pass
+class TestDay01(unittest.TestCase): pass
 
 @Part1(2, "35749")
 @Part2(2, "9365C")
-class TestDay2(unittest.TestCase): pass
+class TestDay02(unittest.TestCase): pass
 
 @Part1(3, 993)
 @Part2(3, 1849)
-class TestDay3(unittest.TestCase): pass
+class TestDay03(unittest.TestCase): pass
 
 @Part1(4, 173787)
 @Part2(4, 548)
-class TestDay4(unittest.TestCase): pass
+class TestDay04(unittest.TestCase): pass
 
 @Part1(5, "4543c154", skip=True)
 @Part2(5, "1050cbbd", skip=True)
-class TestDay5(unittest.TestCase): pass
+class TestDay05(unittest.TestCase): pass
 
 @Part1(6, "afwlyyyq")
 @Part2(6, "bhkzekao")
-class TestDay6(unittest.TestCase): pass
+class TestDay06(unittest.TestCase): pass
 
 @Part1(7, 110)
 @Part2(7, 242)
-class TestDay7(unittest.TestCase): pass
+class TestDay07(unittest.TestCase): pass
 
 @Part1(8, 121)
 @Part2(8,
@@ -39,11 +39,11 @@ class TestDay7(unittest.TestCase): pass
 # #  #  # # #  #  # #  # #    #  # #      #  #    
 #  #  ##  #  #  ##   ##  ####  ##  ####  ### #### 
 """)
-class TestDay8(unittest.TestCase): pass
+class TestDay08(unittest.TestCase): pass
 
 @Part1(9, 120765, skip=True)
 @Part2(9, 11658395076, skip=True)
-class TestDay9(unittest.TestCase): pass
+class TestDay09(unittest.TestCase): pass
 
 @Part1(10, 86)
 @Part2(10, 22847)
