183a465fc1b35790619fe5e3ebe83c5902ddda34
Jonathan Gilchrist <mail@jgilchrist.uk>
2017-12-01 16:24:23 +0000

    Make the solution for day 1 slightly more elegant
    Instead of appending the last character, we can zip the original list
    with itself, rotated by n characters for different problems.

--- 2017/01.py
+++ 2017/01.py
@@ -3,14 +3,16 @@ from utils import *
 from itertools import cycle
 
 def part1(i):
-    return sum_of_matching_pairs(window(i+i[0], 2))
+    return sum_of_matching_pairs(i, rotate_by(i, 1))
 
 def part2(i):
-    rotated_i = cycle(i)
-    for _ in range(len(i)//2):
-        next(rotated_i)
+    return sum_of_matching_pairs(i, rotate_by(i, len(i)//2))
 
-    return sum_of_matching_pairs(zip(i, rotated_i))
+def rotate_by(i, n):
+    i = cycle(i)
+    for _ in range(n):
+        next(i)
+    return i
 
-def sum_of_matching_pairs(list_of_pairs):
-    return sum(int(a) for (a, b) in list_of_pairs if a == b)
+def sum_of_matching_pairs(seq1, seq2):
+    return sum(int(a) for (a, b) in zip(seq1, seq2) if a == b)
