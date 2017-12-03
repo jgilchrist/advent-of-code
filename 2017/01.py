4ec580b0e9704c3284acf2221a2d045170984057
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2017-12-03 14:54:30 +0000

    Slightly simplify the solution for 2017.01

--- 2017/01.py
+++ 2017/01.py
@@ -3,16 +3,15 @@ from utils import *
 from itertools import cycle
 
 def part1(i):
-    return sum_of_matching_pairs(i, rotate_by(i, 1))
+    return sum_of_matching_pairs(i, 1)
 
 def part2(i):
-    return sum_of_matching_pairs(i, rotate_by(i, len(i)//2))
+    return sum_of_matching_pairs(i, len(i)//2)
 
 def rotate_by(i, n):
     i = cycle(i)
-    for _ in range(n):
-        next(i)
+    take(n, i)
     return i
 
-def sum_of_matching_pairs(seq1, seq2):
-    return sum(int(a) for (a, b) in zip(seq1, seq2) if a == b)
+def sum_of_matching_pairs(seq, n):
+    return sum(int(a) for (a, b) in zip(seq, rotate_by(seq, n)) if a == b)
