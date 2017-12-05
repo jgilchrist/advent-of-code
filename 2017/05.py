096fbb2191c558726c8a2d4f2c3437a4a60b3cda
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2017-12-05 20:37:11 +0000

    Pull out common code in 2017.05

--- 2017/05.py
+++ 2017/05.py
@@ -1,34 +1,19 @@
 from utils import *
 
 def part1(i):
-    instructions = list(i)
-    pc = 0
-    steps = 0
-
-    while True:
-        instruction = instructions[pc]
-        instructions[pc] += 1
-        pc += instruction
-        steps += 1
-
-        if pc < 0 or pc >= len(instructions):
-            break
-
-    return steps
+    return run(i, lambda instruction: 1)
 
 def part2(i):
+    return run(i, lambda instruction: -1 if instruction >= 3 else 1)
+
+def run(i, instruction_diff_fn):
     instructions = list(i)
     pc = 0
     steps = 0
 
     while True:
         instruction = instructions[pc]
-
-        if instruction >= 3:
-            instructions[pc] -= 1
-        else:
-            instructions[pc] += 1
-
+        instructions[pc] += instruction_diff_fn(instruction)
         pc += instruction
         steps += 1
 
