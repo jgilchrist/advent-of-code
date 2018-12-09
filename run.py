339f2ebb6fbdba38d054c378cf325033a59a424c
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2018-12-09 14:42:34 +0000

    Add millisecond timings to each challenge run

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from challenge_utils import *
+import time
 
 class Color:
     Red = '\033[91m'
@@ -14,16 +15,28 @@ def yellow(t): return f'{Color.Yellow}{t}{Color.Reset}'
 def green(t): return f'{Color.Green}{t}{Color.Reset}'
 def blue(t): return f'{Color.Blue}{t}{Color.Reset}'
 
+current_milli_time = lambda: int(round(time.time() * 1000))
+
 def run_challenge(year, challenge_number):
     challenge, challenge_input = get_challenge(year, challenge_number)
 
     print(f'{red("=")}{green("=")} Challenge {yellow(challenge_number)}')
 
     print(f'{red("1")}: ', end="")
-    print(challenge.part1(challenge_input))
+    c1_start = current_milli_time()
+    c1_result = challenge.part1(challenge_input)
+    c1_end = current_milli_time()
+    c1_duration = c1_end - c1_start
+    print(c1_result, end="")
+    print(blue(f' ({c1_duration}ms)'))
 
     print(f'{green("2")}: ', end="")
-    print(challenge.part2(challenge_input))
+    c2_start = current_milli_time()
+    c2_result = challenge.part2(challenge_input)
+    c2_end = current_milli_time()
+    c2_duration = c2_end - c2_start
+    print(c2_result, end="")
+    print(blue(f' ({c2_duration}ms)'))
 
 if __name__ == '__main__':
     challenges = range(1, 26)
