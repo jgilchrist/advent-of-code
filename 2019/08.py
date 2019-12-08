3f2d7133d327b644a938a74fb5a9ce3619f104fa
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2019-12-08 11:44:59 +0000

    Clean up 2019.8
    Remove decision about ignoring transparent pixels to the line about
    picking the most significant pixel, which makes sense.
    
    Also move formatting for the terminal to the final line which deals with
    the rest of the formatting.

--- 2019/08.py
+++ 2019/08.py
@@ -9,20 +9,18 @@ def part1(layers):
     return layer_with_least_zeros.count('1') * layer_with_least_zeros.count('2')
 
 def part2(layers):
-    # Grab the pixel from each layer, ignoring 2 (transparent) pixels
-    pixels = ["".join(layer[pixel_index] for layer in layers if layer[pixel_index] != '2')
+    # Grab pixels from every layer for each pixel
+    pixels = ["".join(layer[pixel_index] for layer in layers)
               for pixel_index in range(pixels_per_image)]
 
     # Grab the most significant pixel from each set of pixels for layers
-    pixels = [layer[0] for layer in pixels]
-
-    # Replace pixels with terminal-friendly characters
-    pixels = [layer.replace('0', ' ').replace('1', '*') for layer in pixels]
+    pixels = [next(filter(lambda x: x != '2', layer)) for layer in pixels]
 
     # Divide the pixels into lines
     pixel_lines = list(chunk_list(pixels, image_width))
 
-    result = "\n" + "\n".join("".join(line) for line in pixel_lines)
+    # Format for the terminal
+    result = "\n" + "\n".join("".join(line).replace('0', ' ').replace('1', '*') for line in pixel_lines)
     return result
 
 def transform_input(i):
