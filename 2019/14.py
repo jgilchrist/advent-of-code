fc923e76cc5dca2bafbfc4289ca5b79345cef40e
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2020-12-03 18:40:22 +0000

    WIP

--- 2019/14.py
+++ 2019/14.py
@@ -1,10 +1,48 @@
 from utils import *
 
 Reaction = namedtuple('Reaction', ['before', 'after'])
-Element = namedtuple('Element', ['n', 'name'])
+Element = namedtuple('Element', ['name', 'n'])
+
+def amounts_equal(a, b):
+    a = { k: v for k, v in a.items() if value != 0 }
+    b = { k: v for k, v in b.items() if value != 0 }
+    return a == b
+
+def can_react(contents, reaction):
+    before, after = reaction
+    ingredient, nrequired = reaction.after
+    return contents[ingredient] >= nrequired
+
+def react(contents, reaction):
+    assert can_react(contents, reaction)
+    before, after = reaction
+    new_contents = defaultdict(int, contents)
+    new_contents[after.name] -= after.n
+    if new_contents[after.name] == 0:
+        new_contents.pop(after.name, None)
+
+    for element in before:
+        new_contents[element.name] += element.n
+
+    return new_contents
+
+
+# def get_ore_for(have, reactions):
+#     have
+
+#     ore_for_reaction = { 
+#         reaction: get_ore_for(react(have, reaction), reactions)
+#         for reaction in reactions
+#     }
+
+#     best_reaction, ore = argmin(ore_for_reaction)
+
 
 def part1(i):
-    print(i)
+    have = defaultdict(int, { 'FUEL': 1 })
+
+    # while any(k != "ORE" for k in have.keys()):
+    #     have = 
 
 def part2(i):
     pass
@@ -14,7 +52,7 @@ def transform_input(i):
         n, name = el.split()
         n = int(n)
         name = name.strip()
-        return Element(n, name)
+        return Element(name, n)
 
     def parse_line(line):
         before, after = line.split('=>')
@@ -23,3 +61,24 @@ def transform_input(i):
         return Reaction(before_elements, after_element)
 
     return [parse_line(line) for line in i.splitlines()]
+
+def test():
+    # have_ingredients:
+    assert can_react(
+        defaultdict(int, {'A': 1}),
+        Reaction([], Element('A', 1))
+    )
+
+    assert not can_react(
+        defaultdict(int, {'A': 0}),
+        Reaction([], Element('A', 1))
+    )
+
+    print(react(
+        defaultdict(int, {'A': 1}),
+        Reaction([Element('B', 1)], Element('A', 1))
+    ))
+    assert react(
+        defaultdict(int, {'A': 1}),
+        Reaction([Element('B', 1)], Element('A', 1))
+    ) == {'B': 1}
