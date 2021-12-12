f35652599434e8938c08279b0334476f1fbbff71
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2021-12-12 23:33:10 +0000

    Introduce AocYear trait and run_year() function

--- rust/src/y2021/d01.rs
+++ rust/src/y2021/d01.rs
@@ -14,21 +14,19 @@ impl AocSolution for Day01 {
     type Input = Vec<i32>;
     type Output = usize;
 
-    const YEAR: u32 = 2021;
     const DAY: u32 = 01;
 
-    fn process_input(&self, input: &str) -> Self::Input {
+    fn process_input(input: &str) -> Self::Input {
         input.lines().map(|l| l.parse::<i32>().unwrap()).collect()
     }
 
     const PART1_SOLUTION: Option<Self::Output> = Some(1184);
-    fn part1(&self, i: &Self::Input) -> Self::Output {
+    fn part1(i: &Self::Input) -> Self::Output {
         number_of_positive_diffs(i)
     }
 
     const PART2_SOLUTION: Option<Self::Output> = Some(1158);
-    fn part2(&self, i: &Self::Input) -> Self::Output {
+    fn part2(i: &Self::Input) -> Self::Output {
         number_of_positive_diffs(&i.windows(3).map(|w| w.iter().sum()).collect::<Vec<i32>>())
     }
-
 }
