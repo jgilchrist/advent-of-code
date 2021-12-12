add1dda3ab62ea5852688ba4dbe7f961ce6aa7cc
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2021-12-12 22:59:24 +0000

    Add the ability to specify expected values for solutions

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,13 +5,17 @@ mod y2021;
 
 pub trait AocSolution {
     type Input;
-    type Output: Display;
+    type Output: Display + Eq;
 
     const YEAR: u32;
     const DAY: u32;
 
     fn process_input(&self, input: &str) -> Self::Input;
+
+    const PART1_SOLUTION: Option<Self::Output>;
     fn part1(&self, i: &Self::Input) -> Self::Output;
+
+    const PART2_SOLUTION: Option<Self::Output>;
     fn part2(&self, i: &Self::Input) -> Self::Output;
 }
 
@@ -27,11 +31,27 @@ fn run_solution<TSln: AocSolution>(solution: TSln) -> Result<()> {
     let input = std::fs::read_to_string(path)?;
     let processed_input = solution.process_input(&input);
 
-    let part_1_solution = solution.part1(&processed_input);
-    println!("{}", part_1_solution);
+    let part1_solution = solution.part1(&processed_input);
+    println!("{}", part1_solution);
 
-    let part_2_solution = solution.part2(&processed_input);
-    println!("{}", part_2_solution);
+    if let Some(expected_p1_solution) = TSln::PART1_SOLUTION {
+        if part1_solution == expected_p1_solution {
+            println!("Matches")
+        } else {
+            println!("Does Not Match")
+        }
+    }
+
+    let part2_solution = solution.part2(&processed_input);
+    println!("{}", part2_solution);
+
+    if let Some(expected_p2_solution) = TSln::PART2_SOLUTION {
+        if part2_solution == expected_p2_solution {
+            println!("Matches")
+        } else {
+            println!("Does Not Match")
+        }
+    }
 
     Ok(())
 }
