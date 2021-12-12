ac604adf5da66fdc707a90fc078c9111d22e5a74
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2021-12-12 22:52:24 +0000

    AocSolution trait and run_solution() method

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::{fmt::Display, path::PathBuf};
+
+pub trait AocSolution {
+    type Input;
+    type Output: Display;
+
+    const YEAR: u32;
+    const DAY: u32;
+
+    fn process_input(&self, input: &str) -> Self::Input;
+    fn part1(&self, i: &Self::Input) -> Self::Output;
+    fn part2(&self, i: &Self::Input) -> Self::Output;
+}
+
+fn run_solution<TSln: AocSolution>(solution: TSln) -> Result<()> {
+    let path: PathBuf = [
+        "src",
+        &format!("y{}", TSln::YEAR),
+        &format!("d{:0>2}.in", TSln::DAY),
+    ]
+    .iter()
+    .collect();
+
+    let input = std::fs::read_to_string(path)?;
+    let processed_input = solution.process_input(&input);
+
+    let part_1_solution = solution.part1(&processed_input);
+    println!("{}", part_1_solution);
+
+    let part_2_solution = solution.part2(&processed_input);
+    println!("{}", part_2_solution);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    Ok(())
 }
