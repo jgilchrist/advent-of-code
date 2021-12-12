3298e80228c5402dc3c85704bb0e26eb726ea8ae
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2021-12-12 23:33:10 +0000

    Introduce AocYear trait and run_year() function

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,35 +3,122 @@ use std::{fmt::Display, path::PathBuf};
 
 mod y2021;
 
+pub trait AocYear {
+    const YEAR: u32;
+
+    type D01: AocSolution;
+    type D02: AocSolution;
+    type D03: AocSolution;
+    type D04: AocSolution;
+    type D05: AocSolution;
+    type D06: AocSolution;
+    type D07: AocSolution;
+    type D08: AocSolution;
+    type D09: AocSolution;
+    type D10: AocSolution;
+    type D11: AocSolution;
+    type D12: AocSolution;
+    type D13: AocSolution;
+    type D14: AocSolution;
+    type D15: AocSolution;
+    type D16: AocSolution;
+    type D17: AocSolution;
+    type D18: AocSolution;
+    type D19: AocSolution;
+    type D20: AocSolution;
+    type D21: AocSolution;
+    type D22: AocSolution;
+    type D23: AocSolution;
+    type D24: AocSolution;
+    type D25: AocSolution;
+}
+
 pub trait AocSolution {
     type Input;
     type Output: Display + Eq;
 
-    const YEAR: u32;
     const DAY: u32;
+    const SOLVED: bool = true;
 
-    fn process_input(&self, input: &str) -> Self::Input;
+    fn process_input(input: &str) -> Self::Input;
 
     const PART1_SOLUTION: Option<Self::Output>;
-    fn part1(&self, i: &Self::Input) -> Self::Output;
+    fn part1(i: &Self::Input) -> Self::Output;
 
     const PART2_SOLUTION: Option<Self::Output>;
-    fn part2(&self, i: &Self::Input) -> Self::Output;
+    fn part2(i: &Self::Input) -> Self::Output;
 }
 
-fn run_solution<TSln: AocSolution>(solution: TSln) -> Result<()> {
+pub struct Unsolved;
+
+impl AocSolution for Unsolved {
+    type Input = ();
+    type Output = &'static str;
+
+    const DAY: u32 = 0;
+    const SOLVED: bool = false;
+
+    fn process_input(_: &str) -> Self::Input {
+        unimplemented!()
+    }
+
+    const PART1_SOLUTION: Option<Self::Output> = None;
+    fn part1(_: &Self::Input) -> Self::Output {
+        unimplemented!()
+    }
+
+    const PART2_SOLUTION: Option<Self::Output> = None;
+    fn part2(_: &Self::Input) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+fn run_year<TYear: AocYear>() -> Result<()> {
+    run_solution::<TYear, TYear::D01>()?;
+    run_solution::<TYear, TYear::D02>()?;
+    run_solution::<TYear, TYear::D03>()?;
+    run_solution::<TYear, TYear::D04>()?;
+    run_solution::<TYear, TYear::D05>()?;
+    run_solution::<TYear, TYear::D06>()?;
+    run_solution::<TYear, TYear::D07>()?;
+    run_solution::<TYear, TYear::D08>()?;
+    run_solution::<TYear, TYear::D09>()?;
+    run_solution::<TYear, TYear::D10>()?;
+    run_solution::<TYear, TYear::D11>()?;
+    run_solution::<TYear, TYear::D12>()?;
+    run_solution::<TYear, TYear::D13>()?;
+    run_solution::<TYear, TYear::D14>()?;
+    run_solution::<TYear, TYear::D15>()?;
+    run_solution::<TYear, TYear::D16>()?;
+    run_solution::<TYear, TYear::D17>()?;
+    run_solution::<TYear, TYear::D18>()?;
+    run_solution::<TYear, TYear::D19>()?;
+    run_solution::<TYear, TYear::D20>()?;
+    run_solution::<TYear, TYear::D21>()?;
+    run_solution::<TYear, TYear::D22>()?;
+    run_solution::<TYear, TYear::D23>()?;
+    run_solution::<TYear, TYear::D24>()?;
+    run_solution::<TYear, TYear::D25>()?;
+    Ok(())
+}
+
+fn run_solution<TYear: AocYear, TSln: AocSolution>() -> Result<()> {
+    if !TSln::SOLVED {
+        return Ok(());
+    }
+
     let path: PathBuf = [
         "src",
-        &format!("y{}", TSln::YEAR),
+        &format!("y{}", TYear::YEAR),
         &format!("d{:0>2}.in", TSln::DAY),
     ]
     .iter()
     .collect();
 
     let input = std::fs::read_to_string(path)?;
-    let processed_input = solution.process_input(&input);
+    let processed_input = TSln::process_input(&input);
 
-    let part1_solution = solution.part1(&processed_input);
+    let part1_solution = TSln::part1(&processed_input);
     println!("{}", part1_solution);
 
     if let Some(expected_p1_solution) = TSln::PART1_SOLUTION {
@@ -42,7 +129,7 @@ fn run_solution<TSln: AocSolution>(solution: TSln) -> Result<()> {
         }
     }
 
-    let part2_solution = solution.part2(&processed_input);
+    let part2_solution = TSln::part2(&processed_input);
     println!("{}", part2_solution);
 
     if let Some(expected_p2_solution) = TSln::PART2_SOLUTION {
@@ -57,6 +144,6 @@ fn run_solution<TSln: AocSolution>(solution: TSln) -> Result<()> {
 }
 
 fn main() -> Result<()> {
-    run_solution(y2021::Day01)?;
+    run_year::<y2021::Y2021>()?;
     Ok(())
 }
--- rust/src/y2021/mod.rs
+++ rust/src/y2021/mod.rs
@@ -1,3 +1,37 @@
 mod d01;
 
+pub struct Y2021;
+
+impl AocYear for Y2021 {
+    const YEAR: u32 = 2021;
+
+    type D01 = Day01;
+    type D02 = Unsolved;
+    type D03 = Unsolved;
+    type D04 = Unsolved;
+    type D05 = Unsolved;
+    type D06 = Unsolved;
+    type D07 = Unsolved;
+    type D08 = Unsolved;
+    type D09 = Unsolved;
+    type D10 = Unsolved;
+    type D11 = Unsolved;
+    type D12 = Unsolved;
+    type D13 = Unsolved;
+    type D14 = Unsolved;
+    type D15 = Unsolved;
+    type D16 = Unsolved;
+    type D17 = Unsolved;
+    type D18 = Unsolved;
+    type D19 = Unsolved;
+    type D20 = Unsolved;
+    type D21 = Unsolved;
+    type D22 = Unsolved;
+    type D23 = Unsolved;
+    type D24 = Unsolved;
+    type D25 = Unsolved;
+}
+
 pub use d01::Day01;
+
+use crate::{AocYear, Unsolved};
