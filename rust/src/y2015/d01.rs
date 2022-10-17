e9049fc4b1be81c81f1bb17b7d36f7b443cfe032
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-10-17 12:44:29 +0100

    Add solution for 2015.01

---
+++ rust/src/y2015/d01.rs
@@ -0,0 +1,40 @@
+use crate::AocSolution;
+
+pub struct Day01;
+
+impl AocSolution for Day01 {
+    type Input = String;
+    type Output = usize;
+
+    fn get_input() -> &'static str {
+        include_str!("d01.in")
+    }
+
+    fn process_input(input: &str) -> Self::Input {
+        input.to_string()
+    }
+
+    const PART1_SOLUTION: Option<Self::Output> = Some(74);
+    fn part1(input: &Self::Input) -> Self::Output {
+        input.matches('(').count() - input.matches(')').count()
+    }
+
+    const PART2_SOLUTION: Option<Self::Output> = Some(1795);
+    fn part2(input: &Self::Input) -> Self::Output {
+        let mut floor = 0;
+
+        for (i, c) in input.chars().enumerate() {
+            if c == '(' {
+                floor += 1
+            } else {
+                floor -= 1
+            }
+
+            if floor == -1 {
+                return i + 1;
+            }
+        }
+
+        unreachable!()
+    }
+}
