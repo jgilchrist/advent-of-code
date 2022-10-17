f0a3f55fd911be407626e22b1f83f3695fe18db7
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-10-17 12:16:52 +0100

    Add modules for other years
    All solutions are currently `Unsolved`

---
+++ rust/src/y2019/mod.rs
@@ -0,0 +1,31 @@
+pub struct Y2019;
+
+impl AocYear for Y2019 {
+    type D01 = Unsolved;
+    type D02 = Unsolved;
+    type D03 = Unsolved;
+    type D04 = Unsolved;
+    type D05 = Unsolved;
+    type D06 = Unsolved;
+    type D07 = Unsolved;
+    type D08 = Unsolved;
+    type D09 = Unsolved;
+    type D10 = Unsolved;
+    type D11 = Unsolved;
+    type D12 = Unsolved;
+    type D13 = Unsolved;
+    type D14 = Unsolved;
+    type D15 = Unsolved;
+    type D16 = Unsolved;
+    type D17 = Unsolved;
+    type D18 = Unsolved;
+    type D19 = Unsolved;
+    type D20 = Unsolved;
+    type D21 = Unsolved;
+    type D22 = Unsolved;
+    type D23 = Unsolved;
+    type D24 = Unsolved;
+    type D25 = Unsolved;
+}
+
+use crate::{AocYear, Unsolved};
