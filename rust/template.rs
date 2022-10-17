df066aa080e30f82110c858117f33860abdb548f
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-10-17 12:44:10 +0100

    Use clearer variable name for input in template.rs

--- rust/template.rs
+++ rust/template.rs
@@ -17,12 +17,12 @@ impl AocSolution<N> for DayN {
     }
 
     const PART1_SOLUTION: Option<Self::Output> = None;
-    fn part1(i: &Self::Input) -> Self::Output {
+    fn part1(input: &Self::Input) -> Self::Output {
         todo!()
     }
 
     const PART2_SOLUTION: Option<Self::Output> = None;
-    fn part2(i: &Self::Input) -> Self::Output {
+    fn part2(input: &Self::Input) -> Self::Output {
         todo!()
     }
 }
