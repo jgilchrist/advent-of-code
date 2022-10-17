d9b615d5f2c53c0ac2ba3fa867a1755cdf9f5c97
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-10-17 14:10:20 +0100

    Make Vec2 Copy and add an add_assign and ::new() implementation

--- rust/src/utils/vecs.rs
+++ rust/src/utils/vecs.rs
@@ -1,9 +1,15 @@
-use std::ops::Add;
+use std::ops::{Add, AddAssign};
 
-#[derive(PartialEq, Eq, Clone, Hash, Debug)]
+#[derive(PartialEq, Eq, Clone, Copy, Hash, Debug)]
 pub struct Vec2 {
     pub x: i32,
     pub y: i32,
+}
+
+impl Vec2 {
+    pub fn new(x: i32, y: i32) -> Self {
+        Vec2 { x, y }
+    }
 }
 
 impl Add for Vec2 {
@@ -14,5 +20,12 @@ impl Add for Vec2 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
         }
+    }
+}
+
+impl AddAssign for Vec2 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x = self.x + rhs.x;
+        self.y = self.y + rhs.y;
     }
 }
