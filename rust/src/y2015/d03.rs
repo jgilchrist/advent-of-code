73ebbee33749c7c8ed676545172ccd3499e65805
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-10-17 15:01:18 +0100

    Run `cargo fmt` and `cargo clippy`

--- rust/src/y2015/d03.rs
+++ rust/src/y2015/d03.rs
@@ -1,21 +1,22 @@
-use std::collections::{HashSet, HashMap};
 use once_cell::sync::Lazy;
+use std::collections::{HashMap, HashSet};
 
 use crate::{utils::vecs::Vec2, AocSolution};
 
 pub struct Day03;
 
-static DIRECTION_MAP: Lazy<HashMap<char, Vec2>> = Lazy::new(|| HashMap::from([
-    ('^', Vec2::new( 0, -1)),
-    ('v', Vec2::new( 0,  1)),
-    ('<', Vec2::new(-1,  0)),
-    ('>', Vec2::new( 1,  0)),
-]));
+static DIRECTION_MAP: Lazy<HashMap<char, Vec2>> = Lazy::new(|| {
+    HashMap::from([
+        ('^', Vec2::new(0, -1)),
+        ('v', Vec2::new(0, 1)),
+        ('<', Vec2::new(-1, 0)),
+        ('>', Vec2::new(1, 0)),
+    ])
+});
 
 impl AocSolution for Day03 {
     type Input = String;
     type Output = usize;
-
 
     fn get_input() -> &'static str {
         include_str!("d03.in")
@@ -50,13 +51,13 @@ impl AocSolution for Day03 {
         let robo_santas_instructions = input.chars().skip(1).step_by(2).collect::<Vec<_>>();
 
         for char in santas_instructions.iter() {
-            let direction = DIRECTION_MAP[&char];
+            let direction = DIRECTION_MAP[char];
             santa += direction;
             visited.insert(santa);
         }
 
         for char in robo_santas_instructions.iter() {
-            let direction = DIRECTION_MAP[&char];
+            let direction = DIRECTION_MAP[char];
             robo_santa += direction;
             visited.insert(robo_santa);
         }
