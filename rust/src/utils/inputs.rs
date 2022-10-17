838e27bee31ab2f6780b45a8c07f2dd4ea0cbf89
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-10-17 13:43:45 +0100

    Add solution for 2015.02

--- rust/src/utils/inputs.rs
+++ rust/src/utils/inputs.rs
@@ -1,6 +1,17 @@
+use regex::Regex;
+
 pub fn comma_separated_integers(s: &str) -> Vec<u32> {
     s.trim()
         .split(',')
         .map(|i| i.parse::<u32>().unwrap())
         .collect()
+}
+
+pub fn positive_numbers(s: &str) -> Vec<u32> {
+    let positive_numbers_regex: Regex = Regex::new(r"\d+").unwrap();
+
+    positive_numbers_regex
+        .find_iter(s)
+        .map(|i| i.as_str().parse::<u32>().unwrap())
+        .collect::<Vec<_>>()
 }
---
+++ rust/src/y2015/d02.rs
@@ -0,0 +1,85 @@
+use crate::{utils::inputs, AocSolution};
+
+pub struct Day02;
+
+pub struct Present {
+    length: u32,
+    width: u32,
+    height: u32,
+}
+
+fn paper_area_required(
+    &Present {
+        length,
+        width,
+        height,
+    }: &Present,
+) -> u32 {
+    let sides = [length * width, width * height, height * length];
+
+    let total_area: u32 = sides.iter().map(|s| s * 2).sum();
+    let extra_paper = sides.iter().min().unwrap();
+
+    total_area + extra_paper
+}
+
+fn ribbon_length_required(
+    &Present {
+        length,
+        width,
+        height,
+    }: &Present,
+) -> u32 {
+    let mut dimensions = [length, width, height];
+    dimensions.sort();
+
+    let (smallest, middle) = if let [smallest, middle, _] = dimensions[..] {
+        (smallest, middle)
+    } else {
+        unreachable!()
+    };
+
+    let smallest_perimeter = (smallest + middle) * 2;
+    let volume = length * width * height;
+
+    smallest_perimeter + volume
+}
+
+impl AocSolution for Day02 {
+    type Input = Vec<Present>;
+    type Output = u32;
+
+    fn get_input() -> &'static str {
+        include_str!("d02.in")
+    }
+
+    fn process_input(input: &str) -> Self::Input {
+        fn parse_line(line: &str) -> Present {
+            let numbers = inputs::positive_numbers(line);
+
+            let (x1, x2, x3) = if let [x1, x2, x3] = numbers[..] {
+                (x1, x2, x3)
+            } else {
+                unreachable!()
+            };
+
+            Present {
+                length: x1,
+                width: x2,
+                height: x3,
+            }
+        }
+
+        input.lines().map(parse_line).collect::<Vec<_>>()
+    }
+
+    const PART1_SOLUTION: Option<Self::Output> = Some(1586300);
+    fn part1(input: &Self::Input) -> Self::Output {
+        input.iter().map(paper_area_required).sum()
+    }
+
+    const PART2_SOLUTION: Option<Self::Output> = Some(3737498);
+    fn part2(input: &Self::Input) -> Self::Output {
+        input.iter().map(ribbon_length_required).sum()
+    }
+}
