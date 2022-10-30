8261548a335ee67cf24fc0c3eb635cd1aa140ffc
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-10-30 12:02:38 +0000

    Replace nom with regexes

--- rust/src/utils/inputs.rs
+++ rust/src/utils/inputs.rs
@@ -1,5 +1,58 @@
 use fancy_regex::Regex;
 
+pub struct Captures<'a>(fancy_regex::Captures<'a>);
+
+impl Captures<'_> {
+    pub fn get_string(&self, i: usize) -> String {
+        self.0.get(i).expect("No capture").as_str().to_string()
+    }
+
+    pub fn get_u32(&self, i: usize) -> u32 {
+        self.0
+            .get(i)
+            .expect("No capture")
+            .as_str()
+            .parse::<u32>()
+            .expect("Unable to parse to u32")
+    }
+
+    pub fn get_i32(&self, i: usize) -> i32 {
+        self.0
+            .get(i)
+            .expect("No capture")
+            .as_str()
+            .parse::<i32>()
+            .expect("Unable to parse to i32")
+    }
+}
+
+pub fn transform_lines_by_regex<T>(
+    input: &str,
+    regexes: Vec<(&str, Box<dyn Fn(Captures) -> T>)>,
+) -> Vec<T> {
+    let compiled_regexes = regexes
+        .into_iter()
+        .map(|(re, x)| (Regex::new(re).unwrap(), x))
+        .collect::<Vec<_>>();
+
+    let transformed_lines = input
+        .lines()
+        .map(|l| transform_line_by_regex(&compiled_regexes, l))
+        .collect::<Vec<_>>();
+
+    transformed_lines
+}
+
+fn transform_line_by_regex<T>(regexes: &Vec<(Regex, Box<dyn Fn(Captures) -> T>)>, line: &str) -> T {
+    for (regex, transform_fn) in regexes.iter() {
+        if let Ok(captures) = regex.captures(line) {
+            return transform_fn(Captures(captures.unwrap()));
+        }
+    }
+
+    panic!("Did not match any regexes: \"{}\"", line);
+}
+
 pub fn comma_separated_integers(s: &str) -> Vec<u32> {
     s.trim()
         .split(',')
