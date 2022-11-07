13516b045d4bb656df42664cfa2ac56ad9ef02f9
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-11-07 22:00:06 +0000

    2015.10

--- rust/src/y2015/d10.rs
+++ rust/src/y2015/d10.rs
@@ -1,6 +1,30 @@
+use itertools::Itertools;
+
 use crate::{aoc::Solution, AocSolution};
 
 pub struct Day10;
+
+fn compress_number(number: &str) -> String {
+    let mut chars = number.chars().collect_vec();
+    chars.push(' ');
+
+    let mut result = "".to_string();
+    let mut count = 1;
+
+    for window in chars.windows(2) {
+        let [last_char, current_char] = window else { unreachable!() };
+
+        if current_char == last_char {
+            count += 1
+        } else {
+            result.push_str(&count.to_string());
+            result.push_str(&last_char.to_string());
+            count = 1;
+        }
+    }
+
+    result.to_string()
+}
 
 impl AocSolution for Day10 {
     fn get_input() -> &'static str {
@@ -9,20 +33,30 @@ impl AocSolution for Day10 {
 
     type Input = String;
     fn process_input(input: &str) -> Self::Input {
-        input.lines().collect()
+        input.trim().to_string()
     }
 
     type Part1Output = usize;
-    const PART1_SOLUTION: Solution<Self::Part1Output> =
-        Solution::UnsolvedWithKnownAnswerFromPython(252594);
-    fn part1(_input: &Self::Input) -> Self::Part1Output {
-        todo!()
+    const PART1_SOLUTION: Solution<Self::Part1Output> = Solution::Solved(252594);
+    fn part1(input: &Self::Input) -> Self::Part1Output {
+        let mut s = input.clone();
+
+        for _ in 0..40 {
+            s = compress_number(&s);
+        }
+
+        s.len()
     }
 
     type Part2Output = usize;
-    const PART2_SOLUTION: Solution<Self::Part2Output> =
-        Solution::UnsolvedWithKnownAnswerFromPython(3579328);
-    fn part2(_input: &Self::Input) -> Self::Part2Output {
-        todo!()
+    const PART2_SOLUTION: Solution<Self::Part2Output> = Solution::Solved(3579328);
+    fn part2(input: &Self::Input) -> Self::Part2Output {
+        let mut s = input.clone();
+
+        for _ in 0..50 {
+            s = compress_number(&s);
+        }
+
+        s.len()
     }
 }
