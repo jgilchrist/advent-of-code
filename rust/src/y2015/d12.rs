5caefd853d311b5ac2cab75b9eb73c072269f5b1
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-11-08 18:01:03 +0000

    2015.12

---
+++ rust/src/y2015/d12.rs
@@ -0,0 +1,66 @@
+use crate::{aoc::Solution, AocSolution};
+
+pub struct Day12;
+
+fn count_numbers(json: &serde_json::Value) -> i32 {
+    match json {
+        serde_json::Value::Null => 0,
+        serde_json::Value::Bool(_) => 0,
+        serde_json::Value::Number(n) => n.as_i64().unwrap() as i32,
+        serde_json::Value::String(_) => 0,
+        serde_json::Value::Array(elems) => elems.iter().map(count_numbers).sum(),
+        serde_json::Value::Object(obj) => obj
+            .iter()
+            .map(|(_, prop_value)| count_numbers(prop_value))
+            .sum(),
+    }
+}
+
+fn count_numbers_ignoring_red(json: &serde_json::Value) -> i32 {
+    match json {
+        serde_json::Value::Null => 0,
+        serde_json::Value::Bool(_) => 0,
+        serde_json::Value::Number(n) => n.as_i64().unwrap() as i32,
+        serde_json::Value::String(_) => 0,
+        serde_json::Value::Array(elems) => elems
+            .iter()
+            .map(count_numbers_ignoring_red)
+            .sum(),
+        serde_json::Value::Object(obj) => {
+            let contains_red = obj
+                .iter()
+                .any(|(_, value)| value.is_string() && value.as_str().unwrap() == "red");
+
+            if contains_red {
+                0
+            } else {
+                obj.into_iter()
+                    .map(|(_, prop_value)| count_numbers_ignoring_red(prop_value))
+                    .sum()
+            }
+        }
+    }
+}
+
+impl AocSolution for Day12 {
+    fn get_input() -> &'static str {
+        include_str!("d12.in")
+    }
+
+    type Input = serde_json::Value;
+    fn process_input(input: &str) -> Self::Input {
+        serde_json::from_str(input).unwrap()
+    }
+
+    type Part1Output = i32;
+    const PART1_SOLUTION: Solution<Self::Part1Output> = Solution::Solved(119433);
+    fn part1(input: &Self::Input) -> Self::Part1Output {
+        count_numbers(input)
+    }
+
+    type Part2Output = i32;
+    const PART2_SOLUTION: Solution<Self::Part2Output> = Solution::Solved(68466);
+    fn part2(input: &Self::Input) -> Self::Part2Output {
+        count_numbers_ignoring_red(input)
+    }
+}
--- rust/src/y2015/mod.rs
+++ rust/src/y2015/mod.rs
@@ -9,6 +9,7 @@ mod d08;
 mod d09;
 mod d10;
 mod d11;
+mod d12;
 
 pub struct Y2015;
 
@@ -24,7 +25,7 @@ impl AocYear for Y2015 {
     type D09 = d09::Day09;
     type D10 = d10::Day10;
     type D11 = d11::Day11;
-    type D12 = Unsolved;
+    type D12 = d12::Day12;
     type D13 = Unsolved;
     type D14 = Unsolved;
     type D15 = Unsolved;
