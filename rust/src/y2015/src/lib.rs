6ffacae49449c7ac412e8dc77b11e97b0dcfc3c7
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-11-30 18:10:37 +0000

    2015.13

--- rust/src/y2015/src/lib.rs
+++ rust/src/y2015/src/lib.rs
@@ -29,6 +29,7 @@ mod d09;
 mod d10;
 mod d11;
 mod d12;
+mod d13;
 
 pub struct Y2015;
 
@@ -45,7 +46,7 @@ impl AocYear for Y2015 {
     type D10 = d10::Day10;
     type D11 = d11::Day11;
     type D12 = d12::Day12;
-    type D13 = Unsolved;
+    type D13 = d13::Day13;
     type D14 = Unsolved;
     type D15 = Unsolved;
     type D16 = Unsolved;
