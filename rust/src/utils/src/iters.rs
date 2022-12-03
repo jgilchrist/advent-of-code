4b089656e96e41ce208591620752c32285a68fc8
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-12-03 17:06:47 +0000

    Replace iters::count_items with Itertools::counts

--- rust/src/utils/src/iters.rs
+++ rust/src/utils/src/iters.rs
@@ -1,19 +1,3 @@
-use std::collections::HashMap;
-use std::hash::Hash;
-
-pub fn count_items<T>(items: &[T]) -> HashMap<&T, u32>
-where
-    T: Eq + Hash,
-{
-    let mut counts = HashMap::new();
-
-    for item in items {
-        *counts.entry(item).or_insert(0) += 1;
-    }
-
-    counts
-}
-
 pub fn transpose<T>(ts: &[Vec<T>]) -> Vec<Vec<T>>
 where
     T: Copy,
