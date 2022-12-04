2f914b9280f99015233103e4277cd04e53e0ca2e
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-12-04 10:49:00 +0000

    2022.04

---
+++ rust/src/y2022/src/d04.rs
@@ -0,0 +1,52 @@
+use prelude::*;
+
+pub struct Day04;
+
+pub struct Range(u32, u32);
+
+impl Range {
+    fn contains_range(&self, other: &Self) -> bool {
+        let Self(s1, e1) = self;
+        let Self(s2, e2) = other;
+        s1 <= s2 && e1 >= e2
+    }
+
+    fn contains(&self, n: u32) -> bool {
+        let &Self(start, end) = self;
+        n >= start && n <= end
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        let &Self(start, end) = other;
+        self.contains(start) || self.contains(end)
+    }
+}
+
+impl AocSolution for Day04 {
+    type Input = Vec<(Range, Range)>;
+    fn process_input(input: &str) -> Self::Input {
+        input
+            .lines()
+            .map(|l| {
+                let [s1, e1, s2, e2] = inputs::n_positive_numbers(l);
+                (Range(s1, e1), Range(s2, e2))
+            })
+            .collect()
+    }
+
+    const PART1_SOLUTION: Solution = solution(602);
+    fn part1(input: &Self::Input) -> impl Into<Solution> {
+        input
+            .iter()
+            .filter(|(r1, r2)| r1.contains_range(r2) || r2.contains_range(r1))
+            .count()
+    }
+
+    const PART2_SOLUTION: Solution = solution(891);
+    fn part2(input: &Self::Input) -> impl Into<Solution> {
+        input
+            .iter()
+            .filter(|(r1, r2)| r1.overlaps(r2) || r2.overlaps(r1))
+            .count()
+    }
+}
--- rust/src/y2022/src/lib.rs
+++ rust/src/y2022/src/lib.rs
@@ -24,12 +24,13 @@ pub struct Y2022;
 mod d01;
 mod d02;
 mod d03;
+mod d04;
 
 impl AocYear for Y2022 {
     type D01 = d01::Day01;
     type D02 = d02::Day02;
     type D03 = d03::Day03;
-    type D04 = Unsolved;
+    type D04 = d04::Day04;
     type D05 = Unsolved;
     type D06 = Unsolved;
     type D07 = Unsolved;
