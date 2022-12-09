5031f1c1c65d4c86d9a3b84c613c9aebce0c7021
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2022-12-09 17:24:20 +0000

    Consolidate part 1 and 2 into a simulate_chain function

--- rust/src/y2022/src/d09.rs
+++ rust/src/y2022/src/d09.rs
@@ -28,38 +28,31 @@ impl AocSolution for Day09 {
 
     const PART1_SOLUTION: Solution = solution(6057);
     fn part1(input: &Self::Input) -> impl Into<Solution> {
-        let mut head = Vec2::new(0, 0);
-        let mut tail = Vec2::new(0, 0);
-
-        let mut visited_coords: HashSet<Vec2> = HashSet::new();
-
-        for &direction in input {
-            head = head.move_in_direction(direction);
-            tail = move_tail(head, tail);
-            visited_coords.insert(tail);
-        }
-
-        visited_coords.len()
+        simulate_chain(input, 2)
     }
 
     const PART2_SOLUTION: Solution = solution(2514);
     fn part2(input: &Self::Input) -> impl Into<Solution> {
-        let mut sections = vec![Vec2::new(0, 0); 10];
-        let mut visited_coords: HashSet<Vec2> = HashSet::new();
+        simulate_chain(input, 10)
+    }
+}
 
-        for &direction in input {
-            // Move the head
-            sections[0] = sections[0].move_in_direction(direction);
+fn simulate_chain(instructions: &[CardinalDirection], len: usize) -> usize {
+    let mut visited_coords: HashSet<Vec2> = HashSet::new();
+    let mut sections = vec![Vec2::new(0, 0); len];
 
-            for idx in 0..9 {
-                sections[idx + 1] = move_tail(sections[idx], sections[idx + 1]);
-            }
+    for &direction in instructions {
+        // Move the head
+        sections[0] = sections[0].move_in_direction(direction);
 
-            visited_coords.insert(*sections.last().unwrap());
+        for idx in 0..len - 1 {
+            sections[idx + 1] = move_tail(sections[idx], sections[idx + 1]);
         }
 
-        visited_coords.len()
+        visited_coords.insert(*sections.last().unwrap());
     }
+
+    visited_coords.len()
 }
 
 fn move_tail(head: Vec2, tail: Vec2) -> Vec2 {
