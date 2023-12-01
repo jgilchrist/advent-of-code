14dd75a53a5d1f38834fef4b4100402be6608bb9
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2023-12-01 12:26:29 +0000

    Remove all clippy lint switches from source code

--- rust/src/y2016/src/lib.rs
+++ rust/src/y2016/src/lib.rs
@@ -1,19 +1,5 @@
 #![feature(array_chunks)]
 #![allow(incomplete_features)]
-#![warn(clippy::pedantic)]
-#![warn(clippy::nursery)]
-#![allow(clippy::approx_constant)]
-#![allow(clippy::blanket_clippy_restriction_lints)]
-#![allow(clippy::cast_possible_truncation)]
-#![allow(clippy::cast_possible_wrap)]
-#![allow(clippy::missing_const_for_fn)]
-#![allow(clippy::unreadable_literal)]
-#![allow(clippy::enum_glob_use)]
-#![allow(clippy::redundant_closure_for_method_calls)]
-#![allow(clippy::module_name_repetitions)]
-#![allow(clippy::too_many_lines)]
-#![allow(clippy::missing_panics_doc)]
-#![allow(clippy::must_use_candidate)]
 
 use aoc::AocYear;
 
