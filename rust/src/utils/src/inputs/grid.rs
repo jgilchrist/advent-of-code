892ec22ba3353efd1b8bef59b4102867f6745321
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2023-12-04 11:01:29 +0000

    WIP

--- rust/src/utils/src/inputs/grid.rs
+++ rust/src/utils/src/inputs/grid.rs
@@ -1,6 +1,24 @@
 use crate::geometry::d2::grid::Grid;
 use itertools::Itertools;
 
+/// Create a `Grid<T>` from a string, where `T` can be created from
+/// a single character.
+///
+/// ```
+/// use utils::geometry::d2::vecs::Vec2;
+/// use utils::inputs::grid_of;
+///
+/// struct Digit(pub u32);
+///
+/// impl From<char> for Digit {
+///     fn from(value: char) -> Self {
+///         Self(value.to_digit(10).unwrap())
+///     }
+/// }
+///
+/// let grid = grid_of::<Digit>("123\n345\n789");
+/// assert_eq!(grid.at(Vec2 { x: 1, y: 1 }).unwrap().0, 4);
+/// ```
 pub fn grid_of<T>(s: &str) -> Grid<T>
 where
     T: From<char>,
